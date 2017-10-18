03172ffc0eed59a6ef529689c8aee26f860f0017
Egor Ermakov <unplaced@gmail.com>
2017-10-18 17:17:49 +0100

    Adding generation of disk js/css files

--- gen.go
+++ gen.go
@@ -1,4 +1,5 @@
 package kommentator
 
+//go:generate npm --prefix frontend/ run build
 //go:generate go-bindata -pkg frontend -prefix frontend -o frontend/bindata.go frontend/dist
 //go:generate go-bindata -pkg migrations -prefix migrations -o migrations/bindata.go migrations/
